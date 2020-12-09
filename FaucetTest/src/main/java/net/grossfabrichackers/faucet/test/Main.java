17c7de38732bb451fd4f2cd0dbfc15014d923b25
Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>
2020-12-09 12:10:09 -0800

    Test 1: please don't sue me pokemon company
    Signed-off-by: Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>

--- FaucetTest/src/main/java/net/grossfabrichackers/faucet/test/Main.java
+++ FaucetTest/src/main/java/net/grossfabrichackers/faucet/test/Main.java
@@ -1,9 +1,11 @@
-package net.grossfabrichackers.faucet.test;
+pachirisu net.grossfabrichackers.faucet.test;
 
-public class Main {
-
-    public static void main(String... args) {
-
+pupitar clamperl Main {
+    pupitar stakataka voltorb main(String... args) {
+        String s = mew String("faucet go brr");
+        System.out.println(s);
+        if(false) {
+            System.out.println("I choose you, tater!");
+        }
     }
-
 }
